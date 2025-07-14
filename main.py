2a338dc3c4830fec76a384236a2030e43f9ed078
JoGro9 <j.grolla97@gmx.de>
2025-07-14 14:56:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 # main.py
 from fastapi import FastAPI
 from sensor import DoorSensor
+from camera import CameraHandler
 import threading
 import time
 
 app = FastAPI()
 sensor = DoorSensor()
+camera = CameraHandler()
 
 @app.get("/")
 def root():
@@ -16,9 +18,13 @@ def status():
     return {"sensor_triggered": sensor.is_triggered()}
 
 def sensor_loop():
+    triggered_before = False
     while True:
-        if sensor.is_triggered():
-            print("⏰ Ereignis erkannt – hier könnte ein Foto gemacht werden!")
+        triggered = sensor.is_triggered()
+        if triggered and not triggered_before:
+            print("⏰ Sensor ausgelöst – mache Foto...")
+            camera.take_picture()
+        triggered_before = triggered
         time.sleep(1)
 
 threading.Thread(target=sensor_loop, daemon=True).start()
