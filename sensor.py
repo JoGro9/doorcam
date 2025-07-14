4dc71f935e1d3190efc272a82d2229bddbd5ca83
JoGro9 <j.grolla97@gmx.de>
2025-07-14 14:46:28 +0200

    Update sensor.py

--- sensor.py
+++ sensor.py
@@ -1,8 +1,8 @@
 from gpiozero import Button
 
 def init_sensor():
-    sensor = Button(17)  # BCM GPIO 17 (Pin 11)
-    sensor.when_pressed = sensor_ausgeloest
+    DoorSensor = Button(17)  # BCM GPIO 17 (Pin 11)
+    DoorSensor.when_pressed = sensor_ausgeloest
     print("Magnetsensor ist aktiv.")
 
 def sensor_ausgeloest():
