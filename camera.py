d59d0bbfed33a27191d966f7a03b303360652b1b
JoGro9 <j.grolla97@gmx.de>
2025-07-15 11:04:31 +0200

    Update camera.py

--- camera.py
+++ camera.py
@@ -8,17 +8,19 @@ class CameraHandler:
         self.save_dir = save_dir
         os.makedirs(self.save_dir, exist_ok=True)
         self.picam2 = Picamera2()
-
         # Optional: Kamera konfigurieren
         self.picam2.configure(self.picam2.create_still_configuration())
 
-    def take_picture(self):
-        filename = self._generate_filename()
+    def take_picture(self, filepath=None):
+        if filepath is None:
+            filepath = self._generate_filename()
+        os.makedirs(os.path.dirname(filepath), exist_ok=True)
+
         self.picam2.start()
         time.sleep(2)  # kleine Wartezeit für Auto-Fokus/Belichtung
-        self.picam2.capture_file(filename)
+        self.picam2.capture_file(filepath)
         self.picam2.stop()
-        print(f"Foto aufgenommen: {filename}")
+        print(f"Foto aufgenommen: {filepath}")
 
     def _generate_filename(self):
         timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
