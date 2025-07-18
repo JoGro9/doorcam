d3659bd78da52000628578db5f6b0562e4468505
JoGro9 <j.grolla97@gmx.de>
2025-07-18 15:17:59 +0200

    Update setup_db.py

--- setup_db.py
+++ setup_db.py
@@ -4,12 +4,12 @@ import sqlite3
 conn = sqlite3.connect("faces.db")
 cursor = conn.cursor()
 
-cursor.execute("ALTER TABLE personen ADD COLUMN encoding TEXT")
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS personen (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT,
-    bild TEXT
+    bild TEXT,
+    encoding TEXT
 )
 """)
 
