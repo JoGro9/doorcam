98f5dc1fd6948e1420186588de847efac13a7507
JoGro9 <j.grolla97@gmx.de>
2025-07-18 12:21:32 +0200

    Create setup_db.py

---
+++ setup_db.py
@@ -0,0 +1,17 @@
+# setup_db.py
+import sqlite3
+
+conn = sqlite3.connect("faces.db")
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS personen (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    encoding BLOB,
+    bild BLOB,
+)
+""")
+
+conn.commit()
+conn.close()
