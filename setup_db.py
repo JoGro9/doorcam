a526a9e585dc99dc902aba2cfe4c87afb0834cf1
JoGro9 <j.grolla97@gmx.de>
2025-07-18 15:15:30 +0200

    Update setup_db.py

--- setup_db.py
+++ setup_db.py
@@ -4,7 +4,7 @@ import sqlite3
 conn = sqlite3.connect("faces.db")
 cursor = conn.cursor()
 
-cursor.execute("DROP TABLE IF EXISTS personen")
+cursor.execute("ALTER TABLE personen ADD COLUMN encoding TEXT")
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS personen (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
